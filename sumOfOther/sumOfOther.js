357fc6a2b7523043bc59563345aee1128e797ec5
Dmitry Kestner <kevink2@yandex.ru>
2018-10-28 17:54:03 +0300

    refactor: refactor sumOfOther.js

--- sumOfOther/sumOfOther.js
+++ sumOfOther/sumOfOther.js
@@ -1,11 +1,4 @@
-function sumOfOther(arr){
-  let res = [];
-  let summ = 0;
-  for(let i=0;i<arr.length;i++){
-    summ+=arr[i];
-  }
-  for(let i=0;i<arr.length;i++){
-    res[i] = summ-arr[i];
-  }
-  return res;
-}
+module.exports = function sumOfOther(arr) {
+  const res = arr.reduce((pr, el) => pr + el, 0);
+  return arr.map(el => res - el);
+};
